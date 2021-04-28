b13d61c079300f49463f2fff19a02398c0cdcf03
mayanksonty <mayanksonty@gmail.com>
2021-04-28 11:03:44 +0530

    Its a faulty calculator for some specific input it give wrong output

---
+++ faultyCalc.py
@@ -0,0 +1,29 @@
+op = input("Enter the operator which you want to use: ")
+num1 = int(input("Enter the 1st number"))
+num2 = int(input("Enter the 2nd number"))
+
+if op == '*' and num1 == 45 and num2 == 3:
+    print(555)
+
+elif op == '+' and num1 == 56 and num2 == 9:
+    print(77)
+
+elif op == '/' and num1 == 56 and num2 == 6:
+    print(4)
+
+elif op == '+':
+    print(num1+num2)
+
+elif op == '-':
+    print(num1-num2)
+
+elif op == '*':
+    print(num1*num2)
+
+elif op == '/':
+    print(num1/num2)
+
+elif op == '%':
+    print(mum1%num2)
+
+
